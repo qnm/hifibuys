9f97ed7a99ecd3a62259b3e944e5b287384daef0
Rob Sharp <robsharp@bamboo.(none)>
2009-09-12 08:49:16 +1000

    adding mysql gem dependency

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem 'rubaidh-google_analytics', :lib => 'rubaidh/google_analytics', :source => 'http://gems.github.com'
   config.gem 'builder'
+  config.gem 'mysql'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
