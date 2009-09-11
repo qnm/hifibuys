0ce71d8a8294983b5a3d82659c49be8cb8ec043e
Rob Sharp <robsharp@bamboo.(none)>
2009-09-11 23:25:43 +1000

    Added analytics

--- config/environment.rb
+++ config/environment.rb
@@ -43,3 +43,6 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+Rubaidh::GoogleAnalytics.tracker_id = 'UA-5045550-6'
+Rubaidh::GoogleAnalytics.defer_load = false
