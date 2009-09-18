24c80a29619573d654b6058b548553aa1b32cc97
Rob Sharp <robsharp@bamboo.(none)>
2009-09-18 22:00:20 +1000

    New Carlton Ingestor

---
+++ lib/carlton_ingestor.rb
@@ -0,0 +1,48 @@
+class CarltonIngestor
+
+  require 'hpricot'
+  require 'open-uri'
+  require 'digest/md5'
+
+  attr_accessor :data
+  attr_accessor :hash
+  attr_accessor :current_item
+  attr_accessor :container
+  attr_accessor :container_defaults
+
+  def initialize(params, url)
+    @data = open(url) { |f| Hpricot(f) }
+    @hash = Digest::MD5.hexdigest("#{self.class} #{url}").hex
+    @container = eval params['container']['object']
+    @container_defaults = params['container']['defaults'].merge(
+      :shop_hash => @hash)
+  end
+
+  def wipe
+    @container.find_all_by_shop_hash(@hash).each { |item|
+      item.delete
+      puts "Deleted!"
+    }
+  end
+
+  def ingest
+    # table/tr/td/div/table/tr/td/div
+    #                                 table/tbody/tr/td/table/tbody/tr/td/font/span
+    @data.search("/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody").collect {|@current_item| 
+      itemise
+    }.reject { |item| item.nil? }
+  end
+
+  def itemise
+    @container.new(
+      { :name         => (@current_item/"tbody/tr/td/div/a")[0].inner_html.split("-").first.split(",").first.split("WAS").first,
+        :description  => (@current_item/"tr/td/p").map {|element| element.inner_html}.join(" ").gsub("\n"," ").gsub("\""," "),
+        :url          => (@current_item/"tbody/tr/td/div/a").first.attributes['href'],
+        # we gotta grab out the minimum price from the array
+        :price        => "$" + (@current_item/"tbody/tr/td/div/a")[0].inner_html.gsub(",","").scan(/\$([0-9\.,.]{1,})/).map { |x| x.first.to_i }.min.to_s
+
+      }.merge(@container_defaults)
+    ) unless (@current_item/"tbody/tr/td/div/a").inner_html.scan(/\$([0-9\.,.]{1,})/).min.nil?
+  end
+
+end
---
+++ lib/pymble_ingestor.rb
@@ -0,0 +1,47 @@
+class PymbleIngestor
+
+  require 'hpricot'
+  require 'open-uri'
+  require 'digest/md5'
+
+  attr_accessor :data
+  attr_accessor :hash
+  attr_accessor :current_item
+  attr_accessor :container
+  attr_accessor :container_defaults
+
+  def initialize(params, url)
+    @data = open(url) { |f| Hpricot(f) }
+    @hash = Digest::MD5.hexdigest("#{self.class} #{url}").hex
+    @container = eval params['container']['object']
+    @container_defaults = params['container']['defaults'].merge(
+      :shop_hash => @hash)
+  end
+
+  def wipe
+    @container.find_all_by_shop_hash(@hash).each { |item|
+      item.delete
+      puts "Deleted!"
+    }
+  end
+
+  def ingest
+    # table/tr/td/div/table/tr/td/div
+    #                                 table/tbody/tr/td/table/tbody/tr/td/font/span
+    @data.search("table/tr/td/div/table/tr/td/div").collect {|@current_item| 
+      itemise
+    }.reject { |item| item.nil? }
+  end
+
+  def itemise
+    @container.new(
+      { :name         => (@current_item/"/td[2]/h3/a").first.inner_html.gsub(/- Second hand/,""),
+        # :manufacturer => (@current_item/"/td[1]").first.inner_html.strip.gsub(/[",]/,""),
+        :description  => (@current_item/"/td[2]/div").first.inner_html,
+        :url          => (@current_item/"/td[2]/h3/a").first.attributes['href'],
+        :price        => "$" + (@current_item/"/td[3]").inner_html.scan(/\$([0-9\.,.]{1,})/).first.first
+      }.merge(@container_defaults)
+    ) unless (@current_item/"/td[2]").first.nil?
+  end
+
+end
