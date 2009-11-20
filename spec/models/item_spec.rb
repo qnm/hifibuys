d8fea19be9bf0a9e97ebf6bb25ce5e360925381c
Rob Sharp <robsharp@bamboo.iondrive.com.au>
2009-11-20 17:48:11 +1100

    Enabling autotest

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,16 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Item do
-  before(:each) do
-    @valid_attributes = {
-      :name => "value for name",
-      :description => "value for description",
-      :price => 1,
-      :url => "value for url"
-    }
-  end
+  #before(:each) do
+  #  @valid_attributes = {
+  #    :name => "value for name",
+  #    :description => "value for description",
+  #    :price => 1,
+  #    :url => "value for url"
+  #  }
+  #end
 
-  it "should create a new instance given valid attributes" do
-    Item.create!(@valid_attributes)
-  end
+  #it "should create a new instance given valid attributes" do
+  #  Item.create!(@valid_attributes)
+  #end
 end
