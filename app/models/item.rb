6d114726b38fbea4275d4d71dd8a5d0138bf4d8f
Rob Sharp <robsharp@bamboo.iondrive.com.au>
2009-11-27 23:20:36 +1100

    disabling twitter for now

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,19 @@
 class Item < ActiveRecord::Base
   include Synchroniser::Model
 
-  require 'twitter'
+  #require 'twitter'
 
   attr_accessor :group
-  @@oauth = nil
 
   def item
     url
   end
 
   def after_save
-    config = YAML::load(File.open(RAILS_ROOT + "/config/twitter.yml"))
-    httpauth = Twitter::HTTPAuth.new(config["user"], config["password"])
-    client = Twitter::Base.new(httpauth)
-    client.update("#{name} for #{price} : #{url}")
-puts url
+    #config = YAML::load(File.open(RAILS_ROOT + "/config/twitter.yml"))
+    #httpauth = Twitter::HTTPAuth.new(config["user"], config["password"])
+    #client = Twitter::Base.new(httpauth)
+    #client.update("#{name} for #{price} : #{url}")
   end
 
 end
--- lib/tivoli_ingestor.rb
+++ lib/tivoli_ingestor.rb
@@ -16,7 +16,7 @@ class TivoliIngestor
       container.instance_eval{
         self.name =  ((item/"/td[2]/h3/a").first.inner_html.gsub(/- Second hand/,""))
         self.description = ((item/"/td[2]/div").first.inner_html)
-        self.url = ((item/"/td[2]/h3/a").first.attributes['href']).sub(/&zenid=.*$/,'')
+        self.url = ((item/"/td[2]/h3/a").first.attributes['href'])
         self.price = "$" + ((item/"/td[3]").inner_html.scan(/\$([0-9\.,.]{1,})/).first.first)
         self
       }
