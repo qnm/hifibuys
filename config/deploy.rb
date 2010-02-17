ec2350b578046436086383f241f4db88e629cd91
Rob Sharp <robsharp@bamboo.corp.iondrive.com.au>
2010-02-17 18:28:13 +1100

    Correcting deployment path

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,1 +1,4 @@
-server "www.hifibuys.com.au", :app, :web, :db, :primary => true
+set :stages, %w(staging production)
+set :default_stage, "staging"
+set(:deploy_to) {"/opt/apps/#{application}/#{stage}"}
+require 'capistrano/ext/multistage'
