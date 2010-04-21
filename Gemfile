623a53b7f0d4f329e4f7f28b2ec66d62892ba2e0
Rob Sharp <rob@sharp.id.au>
2010-04-21 17:51:11 +1000

    Migrated to bundler

---
+++ Gemfile
@@ -0,0 +1,29 @@
+# include at least one source and the rails gem
+source :gemcutter
+gem "rails", "2.3.5"
+gem "rack"
+gem "hpricot"
+gem "will_paginate"
+gem "yajl-ruby"
+gem "google_analytics"
+gem "oauth"
+gem "hashie"
+gem "mash"
+gem "httparty"
+gem "twitter"
+gem "builder"
+gem "mysql"
+gem "acts-as-taggable-on"
+gem "thinking-sphinx"
+gem "formtastic"
+gem "thin"
+gem "pg"
+gem "ruby-openid"
+gem "openid"
+
+group :test do
+ gem "webrat"
+ gem "rspec"
+ gem "rspec-rails"
+ gem "acts-as-taggable-on"
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,19 +15,19 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "hpricot"
-  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
-  config.gem 'brianmario-yajl-ruby', :lib => 'yajl', :source => 'http://gems.github.com'
-  config.gem 'rubaidh-google_analytics', :lib => 'rubaidh/google_analytics', :source => 'http://gems.github.com'
-  config.gem 'oauth'
-  config.gem 'hashie'
-  config.gem 'mash', :version => '0.0.3'
-  config.gem 'httparty', :version => '0.4.3'
-  config.gem 'jnunemaker-twitter', :lib => 'twitter', :source => 'http://gems.github.com'
-  config.gem 'builder'
-  config.gem 'mysql'
-  config.gem "acts-as-taggable-on", :source => "http://gemcutter.org"
-  config.gem('thinking-sphinx', :lib     => 'thinking_sphinx', :version => '1.3.11')
+#  config.gem "hpricot"
+#  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+#  config.gem 'brianmario-yajl-ruby', :lib => 'yajl', :source => 'http://gems.github.com'
+#  config.gem 'rubaidh-google_analytics', :lib => 'rubaidh/google_analytics', :source => 'http://gems.github.com'
+#  config.gem 'oauth'
+#  config.gem 'hashie'
+#  config.gem 'mash', :version => '0.0.3'
+#  config.gem 'httparty', :version => '0.4.3'
+#  config.gem 'jnunemaker-twitter', :lib => 'twitter', :source => 'http://gems.github.com'
+#  config.gem 'builder'
+#  config.gem 'mysql'
+#  config.gem "acts-as-taggable-on", :source => "http://gemcutter.org"
+#  config.gem('thinking-sphinx', :lib     => 'thinking_sphinx', :version => '1.3.11')
   #config.gem 'ruby-openid'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
