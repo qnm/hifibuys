962d6a09dec67c1bbe2e5c6f5791da14a48864fc
Rob Sharp <rob@sharp.id.au>
2010-06-18 08:24:25 +1000

    Adding test for manufacturers tagging

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,6 +28,19 @@ describe Item do
       :group => "nogroup"
     }
 
+    @valid_attributes_c = {
+      :name => "test_manufacturer test_type 2",
+      :description => "value for description 2",
+      :price => "value for price",
+      :url => "value for url3",
+      :shop_name => "value for shop_name",
+      :shop_address => "value for shop_address",
+      :shop_suburb => "value for shop_suburb",
+      :shop_state => "value for shop_state",
+      :shop_country => "value for shop_country",
+      :group => "nogroup"
+    }
+
   end
 
   it "should create a new instance given valid attributes" do
@@ -39,14 +52,54 @@ describe Item do
 
     @item = Item.create!(@valid_attributes_b)
     @item.manufacturers.first.name.should == "test_manufacturer"
+
+    Item.tagged_with("test_manufacturer").count.should == 1
+
   end
 
-  it "should create a type tag if an entity matches" do
-    Entity.create!(:name => "test_type", :category => "type")
 
-    @item = Item.create!(@valid_attributes_b)
-    @item.types.first.name.should == "test_type"
+  it "should delete a manufacturers tag no more entities match" do
+    Entity.create!(:name => "test_manufacturer", :category => "manufacturer")
+
+    @item_a = Item.create!(@valid_attributes_b)
+    @item_a.manufacturers.first.name.should == "test_manufacturer"
+
+    Item.tagged_with("test_manufacturer").count.should == 1
+
+    @item_a.delete
+
+    Item.tagged_with("test_manufacturer").count.should == 0 
   end
+
+  it "should not delete a manufacturers tag if more entities match" do
+    Entity.create!(:name => "test_manufacturer", :category => "manufacturer")
+
+    @item_a = Item.create!(@valid_attributes_b)
+    @item_a.manufacturers.first.name.should == "test_manufacturer"
+
+    @item_b = Item.create!(@valid_attributes_c)
+    @item_b.manufacturers.first.name.should == "test_manufacturer"
+
+    Item.tagged_with("test_manufacturer").count.should == 2
+
+    @item_a.delete
+
+    Item.tagged_with("test_manufacturer").count.should == 1 
+  end
+
+
+#  it "should create a type tag if an entity matches" do
+#    Entity.create!(:name => "test_type", :category => "type")
+#
+#    @item = Item.create!(@valid_attributes_b)
+#    @item.types.first.name.should == "test_type"
+
+#    Item.tagged_with("test_type").count.should == 1
+
+#  end
+
+#  it "should delete a type tag if no more entities match" do
+#  end
 
   it "should return an exact match search" do
     @item = Item.create!(@valid_attributes_b)
