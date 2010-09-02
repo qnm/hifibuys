f69c3711da383f2794c262583f3851200df89a22
Rob Sharp <rob@sharp.id.au>
2010-09-03 07:48:57 +1000

    Migrating to synchroniser 0.5.1 and removing loads of unused gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 require 'yaml'
 logger = Logger.new(STDOUT)
 
+category = "electronics/hifi"
+
+base = "config/harvester/#{category}"
+
 logger.info "Deleting existing data"
 
 [Item, SyncItem, Shop, Entity].each do |model|
@@ -8,15 +12,15 @@ logger.info "Deleting existing data"
 end
 
 ["manufacturers", "types"].each do |entity|
-  entity_data = File.open("config/#{entity}.yml", "r") { |f| YAML::load (f) }
+  entity_data = File.open("#{base}/entities/#{entity}.yml", "r") { |f| YAML::load (f) }
   Entity.create(entity_data.inject([]) { |v,e| v << {:category => entity.singularize, :name => e} })
 end
 
-shop_data = File.open('config/shops.yml', 'r') { |shops| YAML::load (shops) }
+shop_data = File.open("#{base}/entities/shops.yml", 'r') { |shops| YAML::load (shops) }
 shops = Shop.create(shop_data)
 
 shops.each do |shop|
-  synchroniser = Synchroniser::Config::Params.new("/config/ingestors/#{shop.filename}.yml", shop.filename, shop.defaults).get_sync
+  synchroniser = Synchroniser::Config::Params.new("/#{base}/#{shop.filename}.yml", shop.filename, shop.defaults).get_sync
   synchroniser.sync
 end
 
---
+++ lib/rebel_ingestor.rb
@@ -0,0 +1,40 @@
+class RebelIngestor
+  include Synchroniser::Ingestor::Uri
+
+  require 'nokogiri'
+  require 'cgi'
+
+  def pagination()
+    puts "moo"
+		Nokogiri::HTML(@data).css("#topStatusNav").css("a").inject([]) do |store, node|
+			store << "http://www.rebelsport.com.au" + node['href']
+		end	
+	end
+
+  def ingest(container)
+    # load the google category in a rather inefficient way
+    breadcrumb = Nokogiri::HTML(@data).css('#breadCrumbs').css("a")
+    breadcrumb.shift
+    container.item_type = CGI.escapeHTML(breadcrumb.inject([]) { |var, element| var << element.text }.join(" > "))
+
+    doc = Nokogiri::HTML(@data).css("#browseCell").collect {|item|
+      itemise(item, container.clone) 
+    }.reject { |item| 
+      item.nil? 
+    }
+  end
+
+  def itemise(item, container)
+    container.instance_eval{
+      self.name = name = item.css('.innerBrowseCell').css('div')[2].content.gsub(/\r\n/,"").split("-")[0].strip
+      self.description = name = item.css('.innerBrowseCell').css('div')[2].content.gsub(/\r\n/,"").split("-")[0].strip
+      colour = item.css('.innerBrowseCell').css('div')[2].content.gsub(/\r\n/,"").split("-")[1].to_s.strip
+      self.colour = colour unless colour == name
+      self.url = "http://www.rebelsport.com.au/ecom/rebel/" + item.css("div a")[1]['href']
+      self.price = "$" + item.css('.innerBrowseCell').children[6].content.scan(/\$([0-9\.,.]{1,})/).first.first unless item.css('.innerBrowseCell').children[6].content.scan(/\$([0-9\.,.]{1,})/).first.instance_of?(Array) == false
+      self.image = "http://www.rebelsport.com.au" + item.css("img")[0]['src']
+      self
+    }
+  end
+
+end
