b3ba6178bcbe14d406290ef1c3b68dc6edf21d69
Rob Sharp <rob@sharp.id.au>
2010-09-20 20:53:58 +1000

    Adding entity types to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ logger.info "Deleting existing data"
 harvester = Harvester::Base.new("electronics/hifi")
 harvester.wipe
 
-{"manufacturers" => Entity, "types" => Entity, "shops" => Shop}. each do |name, entity|
+{"manufacturers" => Entity, "types" => Entity, "shops" => Shop, "entity_types" => EntityType}. each do |name, entity|
   logger.info("Populating #{name.pluralize}")
   creations = harvester.populate(name, entity)
   logger.info("Created #{creations.count} #{name.pluralize}")
