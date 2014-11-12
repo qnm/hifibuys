d783e97eb715fa537ff43fbea48cdec6a4800480
Rob Sharp <qnm@fea.st>
2014-11-12 22:01:34 +1100

    Remove widgets

--- app/widgets/search_widget.rb
+++
@@ -1,9 +0,0 @@
-class SearchWidget < Apotomo::Widget
-  helper Ransack::Helpers::FormHelper
-
-  def display
-    @q = Item.search(params[:q])
-    render
-  end
-
-end
--- app/widgets/tag_list_widget.rb
+++
@@ -1,8 +0,0 @@
-class TagListWidget < Apotomo::Widget
-
-  def display(entity)
-    @tags = Item.tag_counts_on(entity.to_sym)
-    render
-  end
-
-end
